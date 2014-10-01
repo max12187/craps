79bb1c6f15390e6a4332090695a7ec4d432ea7e2
Maxwell Lemkin <max12187@aol.com>
2014-10-01 16:43:10 -0400

    fixed var names and cleaned up the code

--- craps.py
+++ craps.py
@@ -1,26 +1,29 @@
 from random import randint
 
 print('come-out')
-r1 = randint(1, 6) #bad use Intention-Revealing Names
-r2 = randint(1, 6) #bad
-v = r1 + r2        #bad use Intention-Revealing Names
-print(v)           #use searchable names
-if v == 7 or v == 11:
+dice1 = randint(1, 6) 
+dice2 = randint(1, 6) 
+diceTotal = dice1 + dice2        
+print(diceTotal)
+
+if diceTotal == 7 or diceTotal == 11:
     print('You win!')
-elif v == 2 or v == 3 or v == 12:
+elif diceTotal == 2 or diceTotal == 3 or diceTotal == 12:
     print('Craps, you lose.')
 else:
-    print('The point is', v)
-    r1 = randint(1, 6)
-    r2 = randint(1, 6)
-    w = r1 + r2   #use destictive names v & w
-    print(w)
-    while w != v and w != 7:
-        r1 = randint(1, 6)
-        r2 = randint(1, 6)
-        w = r1 + r2
-        print(w)
-    if w == 7:
+    print('The point is', diceTotal)
+    dice1 = randint(1, 6)
+    dice2 = randint(1, 6)
+    newDiceTotal = dice1 + dice2   
+    print(newDiceTotal)
+    
+    while newDiceTotal != diceTotal and newDiceTotal != 7:
+        dice1 = randint(1, 6)
+        dice2 = randint(1, 6)
+        newDiceTotal = dice1 + dice2
+        print(newDiceTotal)
+        
+    if newDiceTotal == 7:
         print('Seven out, you lose.')
     else:
         print('Pass, you win!')
